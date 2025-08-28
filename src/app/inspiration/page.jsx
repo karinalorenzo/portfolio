c1f1c0f58a39efbecf4fe7306074752c7d04f40b
karinalorenzo <karinalrg16@gmail.com>
2025-08-29 00:35:05 +0200

    change the background of carrousel

--- src/app/inspiration/page.jsx
+++ src/app/inspiration/page.jsx
@@ -6,28 +6,28 @@ import "swiper/css/pagination";
 
 export default function About() {
   const inspirations = [
-    { src: "/God.jpg", title: "God" },
-    { src: "/nature.jpg", title: "Nature" },
-    { src: "/tech.jpg", title: "Tecnology" },
-    { src: "/photo.jpg", title: "Photo" },
-    { src: "/Titi.jpg", title: "Titi" },
-    { src: "/sky.jpg", title: "Sky" },
-    { src: "/sea.jpg", title: "Sea" },
-    { src: "/Adele.jpg", title: "Adele" },
-    { src: "/code.jpg", title: "Code" },
-    { src: "/sunset.jpg", title: "Sunset" },
-    { src: "/text.jpg", title: "Text" },
-    { src: "/son.jpg", title: "Son" },
-    { src: "/animal.jpg", title: "Animal" },
-    { src: "/home.jpg", title: "Home" },
-    { src: "/Gijón.jpg", title: "Gijón" },
-    { src: "/princess.jpg", title: "Princess" },
-    { src: "/love.jpg", title: "Love" },
-    { src: "/passion.jpg", title: "Passion" },
-    { src: "/nobad-days.jpg", title: "Good days" },
-    { src: "/sunshine.jpg", title: "Sunshine" },
-    { src: "/design.jpg", title: "Design" },
-    { src: "/office.jpg", title: "Oficce" },
+    { src: "/God.jpg", title: "" },
+    { src: "/nature.jpg", title: "" },
+    { src: "/tech.jpg", title: "" },
+    { src: "/photo.jpg", title: "" },
+    { src: "/Titi.jpg", title: "" },
+    { src: "/sky.jpg", title: "" },
+    { src: "/sea.jpg", title: "" },
+    { src: "/Adele.jpg", title: "" },
+    { src: "/code.jpg", title: "" },
+    { src: "/sunset.jpg", title: "" },
+    { src: "/text.jpg", title: "" },
+    { src: "/son.jpg", title: "" },
+    { src: "/animal.jpg", title: "" },
+    { src: "/home.jpg", title: "" },
+    { src: "/Gijón.jpg", title: "" },
+    { src: "/princess.jpg", title: "" },
+    { src: "/love.jpg", title: "" },
+    { src: "/passion.jpg", title: "" },
+    { src: "/nobad-days.jpg", title: "" },
+    { src: "/sunshine.jpg", title: "" },
+    { src: "/design.jpg", title: "" },
+    { src: "/office.jpg", title: "" },
   ];
 
   return (
@@ -47,7 +47,7 @@ export default function About() {
           modules={[Pagination, Autoplay]}
           pagination={{ clickable: true }}
           autoplay={{ delay: 3500 }}
-          spaceBetween={20}
+          spaceBetween={30}
           slidesPerView={1}
           breakpoints={{
             640: { slidesPerView: 2 },
@@ -58,7 +58,7 @@ export default function About() {
         >
           {inspirations.map((item, idx) => (
             <SwiperSlide key={idx} style={{ background: "transparent" }}>
-              <div className="bg-white bg-opacity-10 backdrop-blur-md rounded-2xl shadow-lg overflow-hidden flex flex-col items-center p-4 transition-transform transform hover:scale-105">
+              <div className="bg-transparent bg-opacity-10 backdrop-blur-md rounded-[16px] shadow-lg overflow-hidden flex flex-col items-center p-4 transition-transform transform hover:scale-105">
                 <img
                   src={item.src}
                   alt={item.title}
